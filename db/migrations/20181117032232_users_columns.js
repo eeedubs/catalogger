e226819910887512fb3ea32456e0f8be1e965b17
danvangogh <djameswhite@yahoo.com>
2018-11-17 04:31:53 +0000

    added password column in migration file

---
+++ db/migrations/20181117032232_users_columns.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function (table) {
+    table.string('password');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('password');
+};
