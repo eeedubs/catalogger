5e2270a3a9637a0a0046cfed02e2cf2d4f8dc98c
danvangogh <djameswhite@yahoo.com>
2018-11-17 06:37:37 +0000

    added post route for resource submission

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,16 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
-    })
+  // router.get("/", (req, res) => {
+  //   knex
+  //     .select("*")
+  //     .from("users")
+  //     .then((results) => {
+  //       res.json(results);
+  //   });
+  //   })
 
   router.post("/register", (req, res) => {
-    console.log("noiiice")
     const username = req.body.username;
     const password = req.body.password;
     knex('users')
@@ -28,5 +27,24 @@ module.exports = (knex) => {
       })
   });
 
+
+  router.post("/submit", (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const resourceURL = req.body.resourceURL;
+    const imageURL = req.body.imageURL;
+    knex('resources')
+      .insert({
+        resourceURL: resourceURL,
+        title: title,
+        imageURL: imageURL,
+        description: description,
+      })
+      .then((results) => {
+        res.redirect("/")
+      });
+  })
+
   return router;
+
 }
