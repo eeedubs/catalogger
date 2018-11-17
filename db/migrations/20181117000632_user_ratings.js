21ae6bca2155d0cbea819a47024efab39d1000f6
danvangogh <djameswhite@yahoo.com>
2018-11-17 00:13:24 +0000

    added user_ratings

---
+++ db/migrations/20181117000632_user_ratings.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_ratings', function (table) {
+      table.increments('id');
+      table.integer('user_id').references('users.id');
+      table.integer('resource_id').references('resources.id');
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('user_ratings');
+};
