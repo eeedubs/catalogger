0fcb00f4fca9f92ca5c69160ee885f2c07ece5eb
Keishon Ross <keishonross97@gmail.com>
2018-11-17 23:23:33 +0000

    disables default function of submit button on index page, logs to console the text in each input field including search bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,65 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
 
+  $(function () {
+    //still selects all forms for some reason
+    $form = $("#submitNew");
+    $text = $(".form-control");
 
-  function createPost(resource){
+    $url = $text[1];
+    $title = $text[2];
+    
 
-  }
+    $form.submit((event) => {
+      event.preventDefault();
+      console.log("Button Clicked!");
 
+      $.ajax({
+        method: "POST",
+        url: "/submit",
+        data: $form.serialize()
+      })
+      .then(
+        () => {
+          console.log("wow this is some text ====> : ",
+           `${$text[0].value}, 
+            ${$text[1].value}, 
+            ${$text[2].value}, 
+            ${$text[3].value}, 
+            ${$text[4].value}`);
+        }
+      )
+    })
+  })
   
+  // .done(() => {
+  //   for(id of resources) {
+  //     $("<div>").addClass("resource").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+
+
+  // function createPost({
+  //   id,
+  //   title,
+  //   description,
+  //   resourceURL,
+  //   imageURL
+  // }) {
+
+  //   console.log("Resource Found!", id, title, description,resourceURL, imageURL);
+  // };
+});
+
+
+
+$(document).ready(function($) {
+
+  $(function() {
+    var url = document.location.href;
+    $('.list-group form').each(function() {
+      if (url === this.action + "?") {
+        $(this).children('button.list-group-item').addClass("active");        
+      }
+    });
+  });
 });
