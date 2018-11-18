347ee634a266faf41a4b0effd859d2060b6233b7
Eric Wilson <ericswilson94@gmail.com>
2018-11-18 21:47:57 +0000

    Commit changes before merge from master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,52 @@
 $(() => {
 
+  $(function () {
+    const $button = $("#commentFeed");
+    const $postComment = $("#commentPost");
+    const $commentSection = $("#comments")/*.hide()*/;
+    const $input = $("#commentInput");
+
+    $button.click((event) => {
+      event.preventDefault();
+      $commentSection.slideToggle();
+      $input.focus();
+    })
+
+    $postComment.click((event) => {
+      event.preventDefault();
+      console.log("Button Clicked!");
+    })
+  })
+
   $.ajax({
     method: "GET",
     url: "api/users"
   }).done((resources) => {
     for(resource of resources) {
-      $(`<div class="resource"><img class="card-img-top" src='${resource.imageURL}'><h3>${resource.title}</h3><a href="${resource.resourceURL}">Source</a><p>${resource.description}</p>`).prependTo($(".category"));
+      $(`
+      <div class="resource">
+        <img class="card-img-top" src='${resource.imageURL}'>
+        <h3>
+        ${resource.title} - <a href="${resource.resourceURL}">Source</a>
+        </h3>
+        <p>
+        ${resource.description}
+        </p>
+        <footer>
+          <a class="btn btn-primary">Rate</a>
+          <a class="btn btn-primary">Like</a>
+          <a id="commentFeed" class="btn btn-primary">Comment</a>
+        </footer>
+        <section id="comments" class="comment-container">
+                <form id="submitComment" method="POST" action="/comment">
+                    <textarea type="text" id="commentInput" name="commentInput" placeholder="Type your comment..."></textarea>
+                    <a href="#" id="commentPost" class="btn-primary">Comment</a>
+                </form>
+                <div class="postArea">
+                </div>
+          </section>
+        <div style="clear: both;"></div>
+        `).prependTo($(".category"));
     }
   });
 
