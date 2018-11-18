97ff578fbfbddecc0ccd79185b17b68dec78d904
Eric Wilson <ericswilson94@gmail.com>
2018-11-18 19:52:31 +0000

    Update search.js file to look for resources matching input

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,12 +1,20 @@
 $(() => {
     $(function () {
-        const $searchForm = $('.input-bar .search-form');
-        const $query = window.location.search.substring(1);
+        const searchForm = $('.input-bar .search-form');
+        const query = window.location.search.substring(1);
         console.log('Query variable: ', $query);
-        const $searchInput = $query.split('&');
+        const searchInput = $query.split('&');
         $searchForm.submit((event) => {
-            knex('resources')
-            .select(
+            knex.select().from('resources')
+            .where('title', 'LIKE', `%${searchInput}%`)
+            .orWhere('description', 'LIKE', `%${searchInput}%`)
+            .asCallback(function(err, result){
+                console.log("Searching...");
+                if (err) {
+                    throw err;
+                }
+                console.log(`Found ${result.length} articles by searching for '${command}':`);
+            })
         })
     });
 });
