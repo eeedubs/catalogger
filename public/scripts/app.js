ed7a7a30246d28b183d0c8a3efc0db9aa1692076
Eric Wilson <ericswilson94@gmail.com>
2018-11-20 00:03:21 +0000

    incremental changes to app.js structuring

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 // App.js handles the functionality of various components within the site, without reloading
 // the web page (jQuery - client side)
 $(() => {
-  // 
+
   $(function () {
     //comment social button
     let $buttons = $("div.resource").children(".comment-container").children("form.submitContent").children("button.btn.btn-primary.commentFeed");
@@ -16,105 +16,142 @@ $(() => {
       // event.preventDefault();
       // console.log("Comment toggle button clicked!");
       $commentSection.slideToggle();
-      // $commentInput.slideToggle();
       $commentInput.focus();
     })
 
-    // function createResource (resource){
-    //   var $allResources = $("<div>").addClass("all-resources");
-    //   var $resource = $("<div>").addClass("resource").appendTo($allResources);
-    //   var $img = $("<img>").addClass("card-img-top").attr("src", "`${resource.imageURL}`").appendTo($resource);
-    //   var $title = $("<h3>").text("`${resource.title}` - <a href='`${resource.resourceURL}`'>Source</a></h3>").appendTo($resource);
-    //   var $description = $("<p>").text("`$(resource.description}`").appendTo($resource);
-    //   var $footer = $("<footer>").appendTo($resource);
-    //   var $rateButton = $("<button>").addClass("btn btn-primary").text("Rate").appendTo($footer);
-    //   var $likeButton = $("<button>").addClass("btn btn-primary").text("Like").appendTo($footer);
-    //   var $commentButton = $("<button>").addClass("btn btn-primary commentFeed").text("Comment").appendTo($footer);
-    // }
+    function createResource (resource){
+      var $allResources = $("<div>").addClass("all-resources");
+      var $singleResource = $("<div>").addClass("resource").appendTo($allResources);
+      var $img = $("<img>").addClass("card-img-top").attr("src", "`${resource.imageURL}`").appendTo($singleResource);
+      var $title = $("<h3>").text("`${resource.title}` - <a href='`${resource.resourceURL}`'>Source</a></h3>").appendTo($singleResource);
+      var $description = $("<p>").text("`$(resource.description}`").appendTo($singleResource);
+      var $footer = $("<footer>").appendTo($singleResource);
+      var $rateButton = $("<button>").addClass("btn btn-primary").text("Rate").appendTo($footer);
+      var $likeButton = $("<button>").addClass("btn btn-primary").text("Like").appendTo($footer);
+      var $commentButton = $("<button>").addClass("btn btn-primary commentFeed").text("Comment").appendTo($footer);
+      return $allResources;
+    }
 
-   
-    $.ajax({
-      method: "GET",
-      url: "api/users"
-    }).done((resources) => {
-      for(resource of resources) {
-        // COPIES STRUCTURE FROM _feed.ejs
-        let $newElement = $(`
-          <div class="all-resources">
-            <div class="resource">
-              <img class="card-img-top" src='${resource.imageURL}'>
-              <h3>${resource.title} - <a href="${resource.resourceURL}">Source</a></h3>
-              <p>
-                ${resource.description}
-              </p>
-              <footer>
-                <button class="btn btn-primary">Rate</button>
-                <button class="btn btn-primary">Like</button>
-                <button class="btn btn-primary commentFeed">Comment</button>
-              </footer>
-              <div class="comment-container">
-              </div>
-              <div style="clear: both;">
-              </div>
-            </div>
-          </div>
-        `).prependTo($("section.feed"));
-          // const $commentFeedToggle = $newElement.find(".commentFeed");
-          // console.log("<a class='btn btn-primary commentFeed'>Comment</a> :  ====> ", $commentFeedToggle);
-        }
-      // makeNewEventHandlers(); // might not need
+    let $commentToggleButton = 
+
+    function getResources(resourceData){
+      // get resources for an array of resource data
+      resourceData.forEach(function(resource) {
+        var $resource = createResource(resource);
+        $("section.feed").prepend($resource);
       });
+    };
+
+    function loadResources() {
+      $.ajax({
+        method: "GET",
+        url: "api/users"
+      }).done((resources) => {
+        console.log("Got resources! Rendering...");
+        getResources(resources);
+      })
+      .fail(() => {
+        alert("Error: resources not rendering properly!");
+      });
+    }
+
+
+    // For constructing the resource postings:
+    // $.ajax({
+    //   method: "GET",
+    //   url: "api/users"
+    // }).done((resources) => {
+    //   for(resource of resources) {
+    //     // COPIES STRUCTURE FROM _feed.ejs
+    //     let $newElement = $(`
+    //       <div class="all-resources">
+    //         <div class="resource">
+    //           <img class="card-img-top" src='${resource.imageURL}'>
+    //           <h3>${resource.title} - <a href="${resource.resourceURL}">Source</a></h3>
+    //           <p>
+    //             ${resource.description}
+    //           </p>
+    //           <footer>
+    //             <button class="btn btn-primary">Rate</button>
+    //             <button class="btn btn-primary">Like</button>
+    //             <button class="btn btn-primary commentFeed">Comment</button>
+    //           </footer>
+    //           <div class="comment-container">
+    //           </div>
+    //           <div style="clear: both;">
+    //           </div>
+    //         </div>
+    //       </div>
+    //     `).prependTo($("section.feed"));
+    //     }
+    //   });
 
     // handles the posting of new comments
+    function createComment (comment) {
+      var $eachComment = $("<div>").addClass("comment");
+      var $header = $("<header>").appendTo($eachComment);
+      var $userName = $("<h4>").addClass("username").text("`${eachComment.userId}`").appendTo($header);
+      var $content = $("<p>").text(`${eachComment.newComment}`).appendTo($eachComment);
+      var $footer = $("<footer>").appendTo($eachComment);
+      var $span = $("<span>").addClass("timestamp").text("19 seconds ago").appendTo($footer);
+      return $eachComment;
+    }
 
-   function createComment (comment) {
-        var $container = $("<div>").addClass("comment-container");
-        var $comment = $("<div>").addClass("comment").appendTo($container);
-        var $header = $("<header>").appendTo($comment);
-        var $userName = $("<h4>").addClass("username").text("`${eachComment.userId}`").appendTo($header);
-        var $content = $("<p>").text(`${eachComment.newComment}`).appendTo($comment);
-        var $footer = $("<footer>").appendTo($comment);
-        var $span = $("<span>").addClass("timestamp").text("19 seconds ago").appendTo($footer);
-        
-      }
+    function getComments(commentData){
+      commentData.forEach(function(comment) {
+        var $comment = createComment(comment);
+        $("div.comment-container").prepend($comment);
+      });
+    };
 
-
-
-    $commentForm.click((event) => {
-      event.preventDefault();
-      $commentSection.slideToggle();
-      console.log("Button Clicked!");
+    function loadComments() {
       $.ajax({
         method: "POST",
         url: "api/users"
       }).done((comments) => {
-        for(eachComment of comments){
-          // COPIES STRUCTURE FROM _comments.ejs
-          let $newComment = $(`
-            <div class="comment">
-              <form class="submitComment" method="POST" action="/api/users/comment">
-                <textarea class="commentInput" type="text" name="commentInput" placeholder="Type your comment..."></textarea>
-                <input class="commentPost" type="submit" value="Post">
-              </form>
-            </div>
-            <div class="postArea">
-              <header>
-                <h4 class="username">${eachComment.userId}</h4>
-              </header>
-              <p>
-                ${eachComment.newComment}
-              </p>
-              <footer>
-                <span class="timestamp">
-                  19 seconds ago
-                </span>
-              </footer>
-            </div>
-          `).appendTo("div.commentContainer");
-        }
+        console.log("Got comments! Rendering...");
+        getComments(comments);
       })
-    })
-  });
+      .fail(() => {
+        alert("Error: comments not rendering properly!");
+      });
+    }
+
+      // COPIES STRUCTURE FROM _comments.ejs
+  //   $commentForm.click((event) => {
+  //     event.preventDefault();
+  //     $commentSection.slideToggle();
+  //     console.log("Button Clicked!");
+  //     $.ajax({
+  //       method: "POST",
+  //       url: "api/users"
+  //     }).done((comments) => {
+  //       for(eachComment of comments){
+  //         let $newComment = $(`
+  //           <div class="comment">
+  //             <form class="submitComment" method="POST" action="/api/users/comment">
+  //               <textarea class="commentInput" type="text" name="commentInput" placeholder="Type your comment..."></textarea>
+  //               <input class="commentPost" type="submit" value="Post">
+  //             </form>
+  //           </div>
+  //           <div class="postArea">
+  //             <header>
+  //               <h4 class="username">${eachComment.userId}</h4>
+  //             </header>
+  //             <p>
+  //               ${eachComment.newComment}
+  //             </p>
+  //             <footer>
+  //               <span class="timestamp">
+  //                 19 seconds ago
+  //               </span>
+  //             </footer>
+  //           </div>
+  //         `).appendTo("div.commentContainer");
+  //       }
+  //     })
+  //   })
+  // });
 
     // Handles the naming of the category titles
   function renameCategory() {
