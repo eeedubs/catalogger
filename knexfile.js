be4793195b316a18e3d13f2d133a9db02a886fc4
Eric Wilson <ericswilson94@gmail.com>
2019-03-27 21:41:18 -0700

    Fixed developer seed files

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
       directory: __dirname + '/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds/production'
+      directory: __dirname + '/db/seeds'
     },
     useNullasDefault: true
   }
